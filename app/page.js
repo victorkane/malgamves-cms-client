aebf4bda377a5d7b0a20b1210eae7973fa9d79ea
victorkane <victorkane@gmail.com>
2023-08-25 12:20:25 -0300

    feat(connect client with strapi api): Can get posts from strapi api and list titles on home page

--- app/page.js
+++ app/page.js
@@ -1,11 +1,31 @@
-import PageContainer from "@/components/PageContainer";
-import Hello from "@/components/Hello"
+import Description from "@/components/Description"
+import PageContainer from "@/components/PageContainer"
+import Main from "@/components/Main"
+import Title from "@/components/Title"
 
 export default async function Home() {
- 
+  const data = await getPosts()
+
   return (
     <PageContainer>
-      <Hello />
+      <Main>
+        <Title>words from others (for me)</Title>
+        <Description>a recipe for disaster</Description>
+        <div className="text-red-900">
+          {data.data.map((post) => (
+            <div key={post.attributes.slug}>{post.attributes.title}</div>
+          ))}
+        </div>
+      </Main>
     </PageContainer>
-  );
+  )
+}
+
+async function getPosts() {
+  const res = await fetch(`${process.env.API_URL}/api/posts`)
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data from API")
+  }
+  return res.json()
 }
