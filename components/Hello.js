8b1085f492d30fa39c721e3aa528968a19ec83ea
victorkane <victorkane@gmail.com>
2023-08-25 10:44:08 -0300

    chore(edit): Change welcome message

--- components/Hello.js
+++ components/Hello.js
@@ -1,23 +1,27 @@
-import PageContainer from "@/components/PageContainer";
-import Main from "@/components/Main";
-import Title from "@/components/Title";
-import Description from "@/components/Description";
-import Link from "next/link";
+import PageContainer from "@/components/PageContainer"
+import Main from "@/components/Main"
+import Title from "@/components/Title"
+import Description from "@/components/Description"
+import Link from "next/link"
 
 const Hello = () => {
-    return (
-        <PageContainer>
-        <Main>
-          <Title>
-            welcome to this template, 
-          </Title>
-          <Description>import a bunch of stuff to play with from the components directory.
-            this template was used to create this <Link href="malgamves.dev"> course </Link>
-          </Description>
-        </Main>
-      </PageContainer>
-    );
-  };
-  
-  export default Hello;
-  
+  return (
+    <PageContainer>
+      <Main>
+        <Title>Welcome to our Next.js Strapi client</Title>
+        <Description>
+          Click to see the template used to create this
+          <Link
+            href="https://github.com/malgamves/next-13-website-template"
+            style={{ color: "rgb(239, 68, 68)" }}
+          >
+            {" "}
+            course{" "}
+          </Link>
+        </Description>
+      </Main>
+    </PageContainer>
+  )
+}
+
+export default Hello
