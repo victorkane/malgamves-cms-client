1f353d89a0e1adb9ce4a67d388eed88bbeef3e63
victorkane <victorkane@gmail.com>
2023-08-25 16:28:26 -0300

    Support React Markdown in our Next.js app

--- components/Content.js
+++ components/Content.js
@@ -1,11 +1,10 @@
-
+import ReactMarkdown from "react-markdown"
 const Content = ({ children }) => {
-    return (
-      <span className="ml-6 pb-4 leading-normal text-black text-base">
-        {children}
-      </span>
-    );
-  };
-  
-  export default Content;
-  
+  return (
+    <span className="ml-6 pb-4 leading-normal text-black text-base">
+      <ReactMarkdown>{`${children}`}</ReactMarkdown>
+    </span>
+  )
+}
+
+export default Content
