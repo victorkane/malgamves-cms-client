a1b33b3741bf0a03b7e86d9c6d67e8e414308e05
victorkane <victorkane@gmail.com>
2023-08-25 16:00:16 -0300

    Complete the individual post dynamic page to include all fields for Lesson 10

--- app/[...slug]/page.js
+++ app/[...slug]/page.js
@@ -1,17 +1,46 @@
-import Description from "@/components/Description"
 import PageContainer from "@/components/PageContainer"
-import Main from "@/components/Main"
 import Title from "@/components/Title"
+import Back from "@/components/Back"
+import LeftGrid from "@/components/LeftGrid"
+import RightGrid from "@/components/RightGrid"
+import Content from "@/components/Content"
+import Centered from "@/components/Centered"
+import Description from "@/components/Description"
+import Link from "next/link"
 
 export default async function Blog({ params }) {
   const { slug } = params
   const data = await getPage(slug)
   const post = data.data[0]
+
+  function reformatDate(fullDate) {
+    const date = new Date(fullDate)
+    return date.toDateString().slice(4)
+  }
+
   return (
     <PageContainer>
-      <Main>
-        <Title>{post.attributes.title}</Title>
-      </Main>
+      <LeftGrid>
+        <Centered>
+          <Title>{post.attributes.title}</Title>
+          <Description>
+            by{" "}
+            <span className="text-red-700 italic">
+              {" "}
+              {post.attributes.author}{" "}
+            </span>{" "}
+            on {reformatDate(post.attributes.date)}
+          </Description>
+          <Back>
+            <Link href="/"> &larr; go back home</Link>
+          </Back>
+        </Centered>
+      </LeftGrid>
+      <RightGrid>
+        <Centered>
+          <Content>{post.attributes.body}</Content>
+        </Centered>
+      </RightGrid>
     </PageContainer>
   )
 }
