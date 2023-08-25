a5c7ca6145a2b7612154ff4456466ccd966117f2
victorkane <victorkane@gmail.com>
2023-08-25 12:57:05 -0300

    feat(list component): list and format title, author, date, extract of each post on homepage

--- components/List.js
+++ components/List.js
@@ -1,8 +1,16 @@
-import Link from "next/link";
-import '@/app/globals.css';
+import Link from "next/link"
+import "@/app/globals.css"
 
 const List = ({ allPosts }) => {
-  
+  function truncateSummary(content) {
+    return content.slice(0, 100).trimEnd()
+  }
+
+  function reformatDate(fullDate) {
+    const date = new Date(fullDate)
+    return date.toDateString().slice(4)
+  }
+
   return (
     <>
       <div className="flex left-0 content-start justify-center flex-wrap max-w-lg">
@@ -11,13 +19,24 @@ const List = ({ allPosts }) => {
             <h2 className="ml-6 text-left text-red-500 no-underline pb-1 max-w-md rounded-md font-bold text-2xl">
               <Link href={{ pathname: `/${post.attributes.slug}` }} passHref>
                 {post.attributes.title}
-              </Link>{" "}
+              </Link>
+              <p>
+                by{" "}
+                <span className="text-red-700 italic">
+                  {" "}
+                  {post.attributes.author}{" "}
+                </span>{" "}
+                on {reformatDate(post.attributes.date)}
+              </p>
             </h2>
+            <p className="ml-6 pb-4 text-red-900 font-serif leading-normal">
+              {`${truncateSummary(post.attributes.body)}` + "..."}
+            </p>
           </div>
         ))}
       </div>
     </>
-  );
-};
+  )
+}
 
-export default List;
+export default List
