2ed461b37cd78a75d9bebc0a3090269b5eff49ce
victorkane <victorkane@gmail.com>
2023-08-25 11:43:52 -0300

    build: scaffold about page using app dir

---
+++ app/about/page.js
@@ -0,0 +1,15 @@
+import Description from "@/components/Description"
+import PageContainer from "@/components/PageContainer"
+import Main from "@/components/Main"
+import Title from "@/components/Title"
+
+export default function About() {
+  return (
+    <PageContainer>
+      <Main>
+        <Title>about this</Title>
+        <Description>some info about us</Description>
+      </Main>
+    </PageContainer>
+  )
+}
